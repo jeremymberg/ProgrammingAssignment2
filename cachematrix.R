d0cd6fef7767c5c2faa5316fec9d862f836332e5
jeremymberg <jeremymberg@gmail.com>
2015-05-15 08:05:40 -0400

    Added code for first function makeCacheMatrix which appears to function properly

--- cachematrix.R
+++ cachematrix.R
@@ -9,10 +9,21 @@
 ## a cached response. This is analogous to the cachemean as an example.
 
 ## This function generates a "matrix" including an associate object
-## corresponding to its inverse called "inv"
+## corresponding to its inverse called "inv". The final list includes
+## set, get, setinverse, getinverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
